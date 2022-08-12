8d3a056713bb6086a54f855afc38146334835ac3
Michael Anderson <md.anderson03@gmail.com>
2022-08-12 15:42:21 -0400

    8_11_22 analysis
    Fully analyzed 8_11_22 dataset of A488 decays kinetics with mCPBA

--- testing.py
+++ testing.py
@@ -1,8 +1,17 @@
+import math
+'''
 from autocif import *
 microscope = autocif.cycif()
 
 
 microscope.surf2focused_surf(core, magellan, ['DAPI'])
 microscope.micro_magellan_acq()
+'''
+time = 3850
 
+
+double_value = 43237*(1* math.exp(-0.0009504* time) + 0*math.exp(-0.008874 * time)) + 20568
+
+threshold = 0.05 *(43237 - 20568)
+print(double_value - 20568, threshold)
 
