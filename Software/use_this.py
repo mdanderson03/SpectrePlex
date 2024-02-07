de75ae498a6ac26284cee5dba9e38ca94011382c
mdanderson03 <md.anderson03@gmail.com>
2024-02-06 19:25:40 -0500

    fluidic updates

--- Software/use_this.py
+++ Software/use_this.py
@@ -3,7 +3,7 @@ from optparse import OptionParser
 microscope = cycif() # initialize cycif object
 pump = fluidics(6, 3)
 
-experiment_directory = r'E:\tissue_identify'
+experiment_directory = r'E:\6-1-24 multiplex'
 z_slices = 7
 x_frame_size = 2960
 offset_array = [0, -8, -7, -7]
@@ -24,10 +24,10 @@ offset_array = [0, -8, -7, -7]
 
 
 #pump.liquid_action('Bleach')
-#pump.liquid_action('Wash')
+pump.liquid_action('Wash')
 #microscope.establish_fm_array(experiment_directory, 1, z_slices, offset_array, initialize=1)
 #microscope.fm_channel_initial(experiment_directory, offset_array, z_slices)
-microscope.image_cycle_acquire(1, experiment_directory, z_slices, 'Stain', offset_array, x_frame_size=x_frame_size, establish_fm_array=0, auto_focus_run=1, auto_expose_run=0)
+# microscope.image_cycle_acquire(0, experiment_directory, z_slices, 'Bleach', offset_array, x_frame_size=x_frame_size, establish_fm_array=1, auto_focus_run=0, auto_expose_run=0)
 
 
 #microscope.recursive_stardist_autofocus(experiment_directory, 1)
@@ -37,9 +37,9 @@ microscope.image_cycle_acquire(1, experiment_directory, z_slices, 'Stain', offse
 
 #print(microscope.kinetic_autofocus(experiment_directory, -87, 11))
 
-#for cycle in range(0,8):
-
- #   microscope.full_cycle(experiment_directory, cycle, offset_array, cycle, pump, z_slices, incub_val=45)
+# for cycle in range(1,8):
+#
+#    microscope.full_cycle(experiment_directory, cycle, offset_array, cycle, pump, z_slices, incub_val=45)
 
 #microscope.post_acquisition_processor(experiment_directory, x_frame_size)
 #microscope.stage_placement(experiment_directory, 1, x_frame_size)
