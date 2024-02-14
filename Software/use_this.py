49ef52db995d87e165accb571e28ccb9a96f649c
Michael Anderson <md.anderson03@gmail.com>
2024-02-14 13:39:12 -0500

    logger and pressure mode
    added in logger and ability to run in pressure mode

--- Software/use_this.py
+++ Software/use_this.py
@@ -1,6 +1,6 @@
 from autocyplex import *
 from optparse import OptionParser
-microscope = cycif() # initialize cycif object
+#microscope = cycif() # initialize cycif object
 pump = fluidics(6, 3)
 
 experiment_directory = r'E:\tissue_identify'
@@ -8,54 +8,48 @@ z_slices = 7
 x_frame_size = 2960
 offset_array = [0, -8, -7, -7]
 
-#microscope.full_cycle(experiment_directory, cycle, offset_array, cycle, pump, z_slices)
-
-
-
-#pump.liquid_action('Stain', stain_valve = 1, incub_val=45)
-#pump.liquid_action('Bleach')
-#microscope.image_cycle_acquire(1, experiment_directory, z_slices, 'Bleach', offset_array, x_frame_size=x_frame_size, establish_fm_array=0, auto_focus_run=0, auto_expose_run=0)
-#microscope.establish_fm_array(experiment_directory, 4, z_slices, offset_array, initialize=0,x_frame_size=x_frame_size, autofocus=0, auto_expose=1)
-
-#print(core.get_position())
-
-#microscope.image_cycle_acquire(0, experiment_directory, z_slices, 'Bleach', offset_array, x_frame_size=x_frame_size, establish_fm_array=1, auto_focus_run=0, auto_expose_run=0)
-#microscope.image_cycle_acquire(4, experiment_directory, z_slices, 'Stain', offset_array, x_frame_size=x_frame_size, establish_fm_array=0, auto_focus_run=0, auto_expose_run=1)
-
-
-#pump.liquid_action('Bleach')
-#pump.liquid_action('Wash')
-#microscope.image_cycle_acquire(1, experiment_directory, z_slices, 'Stain', offset_array, x_frame_size=x_frame_size, establish_fm_array=0, auto_focus_run=1, auto_expose_run=0)
-
 
 pump.liquid_action('Stain', stain_valve=1, incub_val=5)
 pump.liquid_action('Wash')
 pump.liquid_action('Bleach')
 pump.liquid_action('Wash')
 
+pump.liquid_action('Stain', stain_valve=2, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
 
-#microscope.recursive_stardist_autofocus(experiment_directory, 1)
+pump.liquid_action('Stain', stain_valve=3, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
 
-#microscope.antibody_kinetics(experiment_directory, 0.5, 90, 1, pump)
-#microscope.antibody_kinetics(experiment_directory, 1, 2, 1, pump)
+pump.liquid_action('Stain', stain_valve=4, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
 
-#print(microscope.kinetic_autofocus(experiment_directory, -87, 11))
+pump.liquid_action('Stain', stain_valve=5, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
+
+pump.liquid_action('Stain', stain_valve=6, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
+
+pump.liquid_action('Stain', stain_valve=7, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
+
+pump.liquid_action('Stain', stain_valve=8, incub_val=5)
+pump.liquid_action('Wash')
+pump.liquid_action('Bleach')
+pump.liquid_action('Wash')
 
 
-#for cycle in range(0,1):
 
-#  microscope.full_cycle(experiment_directory, cycle, offset_array, cycle, pump, z_slices, incub_val=45)
-
-#microscope.post_acquisition_processor(experiment_directory, x_frame_size)
-#microscope.stage_placement(experiment_directory, 1, x_frame_size)
-
-#microscope.mcmicro_image_stack_generator(1, experiment_directory, x_frame_size)
-
-#experiment_directory = r'E:\11-1-24 test kinetics'
-#offset_array = [0, -8, -7, -7]
-
-#z_slices = 9
-#x_frame_size = 2960
-#cycle = 0
 
 
