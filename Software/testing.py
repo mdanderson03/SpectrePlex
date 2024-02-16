f7e45fa1e2aa186dfa8fa403b52754ff6f0d1d87
M.D. Anderson <48689483+mdanderson03@users.noreply.github.com>
2024-02-16 10:07:18 -0500

    Update testing.py

--- Software/testing.py
+++ Software/testing.py
@@ -4,14 +4,5 @@ from skimage import io, morphology
 from matplotlib import pyplot as plt
 import os
 
-experiment_directory = r'E:\tissue_identify\Tissue_Binary'
-os.chdir(experiment_directory)
-file_name = 'x3_y_0_tissue.tif'
-image = io.imread(file_name)
-image = image.astype('bool')
-
-image_2 = morphology.remove_small_objects(image, min_size=80000, connectivity=1)
-image_2 = image_2.astype('int8')
-
-io.imshow(image_2)
-io.show()
+file_directory = r'C:\Users\mdand\OneDrive\Desktop'
+filename = r''
