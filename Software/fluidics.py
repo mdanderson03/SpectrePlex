5c380dc9d1f03429aabf3e8a44c3cb0373fe7f91
M.D. Anderson <48689483+mdanderson03@users.noreply.github.com>
2024-02-16 12:53:55 -0500

    Update fluidics.py
    added end and start ob1 with loggers

--- Software/fluidics.py
+++ Software/fluidics.py
@@ -25,16 +25,14 @@ class fluidics:
         # OB1 initialize
         ob1_path = 'ASRL' + str(ob1_com_port) + '::INSTR'
         Instr_ID = c_int32()
-        pump = OB1_Initialization(ob1_path.encode('ascii'), 0, 0, 0, 0, byref(Instr_ID))
-        pump = OB1_Add_Sens(Instr_ID, 1, 5, 1, 0, 7,
+        OB1_Initialization(ob1_path.encode('ascii'), 0, 0, 0, 0, byref(Instr_ID))
+        OB1_Add_Sens(Instr_ID, 1, 5, 1, 0, 7,
                             0)  # 16bit working range between 0-1000uL/min, also what are CustomSens_Voltage_5_to_25 and can I really choose any digital range?
 
         Calib_path = r'C:\Users\CyCIF PC\Documents\GitHub\AutoCIF\Python_64_elveflow\calibration\1_12_24_cal.txt'
         Calib = (c_double * 1000)()
         Elveflow_Calibration_Load(Calib_path.encode('ascii'), byref(Calib), 1000)
         #Elveflow_Calibration_Default(byref(Calib), 1000)
-        OB1_Start_Remote_Measurement(Instr_ID.value, byref(Calib), 1000)
-        self.calibration_array = byref(Calib)
 
         if flow_control == 1:
 
@@ -45,6 +43,9 @@ class fluidics:
             PID_Add_Remote(Instr_ID.value, set_channel_regulator, Instr_ID.value, set_channel_sensor, 0.9, 0.004, 1)
         else:
             pass
+
+        OB1_Start_Remote_Measurement(Instr_ID.value, byref(Calib), 1000)
+        self.calibration_array = byref(Calib)
 
         # MUX intiialize
         path = 'ASRL' + str(mux_com_port) + '::INSTR'
@@ -102,7 +103,9 @@ class fluidics:
 
     def mux_end(self):
 
-        MUX_DRI_Destructor(self.mux_ID)
+
+        error = MUX_DRI_Destructor(self.mux_ID)
+        self.fluidics_logger(str(MUX_DRI_Destructor), error, 0)
 
     def valve_select(self, valve_number):
         '''
@@ -138,7 +141,6 @@ class fluidics:
 
         # OB1_Start_Remote_Measurement(self.pump_ID, self.calibration_array, 1000)
         error = OB1_Set_Remote_Target(self.pump_ID, set_channel, set_target)
-
         self.fluidics_logger(str(OB1_Set_Remote_Target), error, flow_rate)
 
         data_sens = c_double()
@@ -146,6 +148,35 @@ class fluidics:
         set_channel = int(1)  # convert to int
         set_channel = c_int32(set_channel)  # convert to c_int32
         time.sleep(3)
+        error = OB1_Get_Remote_Data(self.pump_ID, set_channel, byref(data_reg), byref(data_sens))
+        current_flow_rate = data_sens.value
+        self.fluidics_logger(str(OB1_Get_Remote_Data), error, current_flow_rate)
+
+        if flow_rate > 400 and current_flow_rate < 0.1 * flow_rate:
+            print('restarting PID loop')
+            #stop and restart PID loop
+            error = PID_Set_Running_Remote(self.pump_ID, set_channel, 0)
+            self.fluidics_logger(str(PID_Set_Running_Remote), error, 0)
+            error = PID_Set_Running_Remote(self.pump_ID, set_channel, 1)
+            self.fluidics_logger(str(PID_Set_Running_Remote), error, 1)
+            # resend flow rate set point
+            error = OB1_Set_Remote_Target(self.pump_ID, set_channel, set_target)
+            self.fluidics_logger(str(OB1_Set_Remote_Target), error, flow_rate)
+
+        if flow_rate < 40 and current_flow_rate > 100:
+            print('restarting PID loop')
+            #stop and restart PID loop
+            error = PID_Set_Running_Remote(self.pump_ID, set_channel, 0)
+            self.fluidics_logger(str(PID_Set_Running_Remote), error, 0)
+            error = PID_Set_Running_Remote(self.pump_ID, set_channel, 1)
+            self.fluidics_logger(str(PID_Set_Running_Remote), error, 1)
+            # resend flow rate set point
+            error = OB1_Set_Remote_Target(self.pump_ID, set_channel, set_target)
+
+
+            self.fluidics_logger(str(OB1_Set_Remote_Target), error, flow_rate)
+
+
         error = OB1_Get_Remote_Data(self.pump_ID, set_channel, byref(data_reg), byref(data_sens))
         current_flow_rate = data_sens.value
         self.fluidics_logger(str(OB1_Get_Remote_Data), error, current_flow_rate)
@@ -160,27 +191,48 @@ class fluidics:
 
     def ob1_end(self):
 
-        set_channel = int(1)  # convert to int
-        set_channel = c_int32(set_channel)  # convert to c_int32
+        error = PID_Set_Running_Remote(self.pump_ID, set_channel, 0)
+        self.fluidics_logger(str(PID_Set_Running_Remote), error, 0)
 
-        data_sens = c_double()
-        data_reg = c_double()
+        error = OB1_Stop_Remote_measurement(self.pump_ID)
+        self.fluidics_logger(str(OB1_Stop_Remote_measurement), error, 0)
 
-        x = 0
-        self.flow(0)
-
-        while x == 0:
-            OB1_Get_Remote_Data(self.pump_ID, set_channel, byref(data_reg), byref(data_sens))
-            flow_rate = data_sens.value
-
-            if flow_rate < 10:
-                x = 1
-            if flow_rate > 10:
-                x = 0
-            time.sleep(1)
-
-        OB1_Stop_Remote_Measurement(self.pump_ID)
         OB1_Destructor(self.pump_ID)
+        self.fluidics_logger(str(OB1_Destructor), error, 0)
+
+    def ob1_start(self, ob1_com_port, flow_control = 1):
+
+        # OB1 initialize
+        ob1_path = 'ASRL' + str(ob1_com_port) + '::INSTR'
+        Instr_ID = c_int32()
+        error = OB1_Initialization(ob1_path.encode('ascii'), 0, 0, 0, 0, byref(Instr_ID))
+        self.fluidics_logger(str(OB1_Initialization), error, 1)
+        error = OB1_Add_Sens(Instr_ID, 1, 5, 1, 0, 7,
+                            0)  # 16bit working range between 0-1000uL/min, also what are CustomSens_Voltage_5_to_25 and can I really choose any digital range?
+        self.fluidics_logger(str(OB1_Add_Sens), error, 1)
+        Calib_path = r'C:\Users\CyCIF PC\Documents\GitHub\AutoCIF\Python_64_elveflow\calibration\1_12_24_cal.txt'
+        Calib = (c_double * 1000)()
+        error = Elveflow_Calibration_Load(Calib_path.encode('ascii'), byref(Calib), 1000)
+        self.fluidics_logger(str(Elveflow_Calibration_Load), error, 1)
+        #Elveflow_Calibration_Default(byref(Calib), 1000)
+
+        if flow_control == 1:
+
+            set_channel_regulator = int(1)  # convert to int
+            set_channel_regulator = c_int32(set_channel_regulator)  # convert to c_int32
+            set_channel_sensor = int(1)
+            set_channel_sensor = c_int32(set_channel_sensor)  # convert to c_int32
+            error = PID_Add_Remote(Instr_ID.value, set_channel_regulator, Instr_ID.value, set_channel_sensor, 0.9, 0.004, 1)
+            self.fluidics_logger(str(PID_Add_Remote), error, 1)
+        else:
+            pass
+
+        error = OB1_Start_Remote_Measurement(Instr_ID.value, byref(Calib), 1000)
+        self.fluidics_logger(str(OB1_Start_Remote_Measurement), error, 1)
+        self.calibration_array = byref(Calib)
+
+        self.pump_ID = Instr_ID.value
+
 
     def measure(self):
 
