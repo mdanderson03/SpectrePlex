94be3463c96bbe2cd163845668f3cc11d107df92
mdanderson03 <md.anderson03@gmail.com>
2024-02-21 09:00:48 -0500

    updates

--- Software/use_this.py
+++ Software/use_this.py
@@ -3,13 +3,12 @@ import datetime
 from autocyplex import *
 from optparse import OptionParser
 microscope = cycif() # initialize cycif object
-experiment_directory = r'E:\20-2-24 high temp ph7 kinetics'
+experiment_directory = r'E:\20-2-2024 healthy multiplex'
 pump = fluidics(experiment_directory, 6, 3, flow_control=1)
 
 z_slices = 7
 x_frame_size = 2960
 offset_array = [0, -8, -7, -7]
 
-#for cycle in range(0,8):
-#    microscope.full_cycle(experiment_directory, cycle, offset_array, cycle,  pump, z_slices)
-microscope.antibody_kinetics(experiment_directory, 0.5, 90, 1, pump)
+for cycle in range(0,8):
+    microscope.full_cycle(experiment_directory, cycle, offset_array, cycle,  pump, z_slices)
