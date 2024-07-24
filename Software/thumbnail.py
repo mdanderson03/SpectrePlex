e912ae6c65d37464ceeb5a590cdb2d6fa81dc7cf
mdanderson03 <md.anderson03@gmail.com>
2024-07-24 08:44:28 -0400

    Update thumbnail.py

--- Software/thumbnail.py
+++ Software/thumbnail.py
@@ -5,43 +5,28 @@ import binascii
 import ipaddress
 import logging
 import socket
-#found_devices = asyncio.run(Discover.discover(target="192.168.0.1"))
-#print(found_devices)
 import numpy as np
 from copy import deepcopy
 from csbdeep.utils import normalize
-from stardist.models import StarDist2D
+
 import os
 from skimage import io
 
-model = StarDist2D.from_pretrained('2D_versatile_fluo')
+a= np.array(([1,1], [0,0]))
+b= np.array(([0,1], [0,1]))
+non_zeros = np.nonzero(b)
+#print(a, b)
+c = np.subtract(a, b)
+d = np.where(c[non_zeros] == 0)
 
+a = np.array([1,2,3,4,5])
+b = np.array([1,2,4,7,9])
+disjoint_a_b = np.setdiff1d(a, b)
+disjoint_b_a = np.setdiff1d(b, a)
+print(disjoint_a_b, disjoint_b_a)
 
-experiment_directory = r'E:\3-7-24 marco'
-z_slices = 3
-x_frame_size = 2960
-offset_array = [0, -7, -7, -6]
-focus_position = 258
+for x in range(0, len(disjoint_a_b)):
+    indicies = np.where(b == disjoint_b_a[x] )
+    b[indicies] = disjoint_a_b[x]
 
-labelled_path = experiment_directory + '/Labelled_Nuc'
-dapi_im_path = r'E:\3-7-24 marco\DAPI\Bleach\cy_0\Tiles'
-z_center_index = 1
-
-
-for x in range(0, 1):
-    for y in range(5, 6):
-        os.chdir(dapi_im_path)
-        file_name = 'z_' + str(z_center_index) + '_x' + str(x) + '_y_' + str(y) + '_c_DAPI.tif'
-        labelled_file_name = 'x' + str(x) + '_y_' + str(y) + '_c_DAPI.tif'
-        img = io.imread(file_name)
-        img = img.astype('int32')
-        print(np.min(img))
-        img[img < 0] = 0
-        #img = skimage.util.img_as_uint(img)
-        labels, _ = model.predict_instances(normalize(img))
-        labels[labels > 0] = 1
-        print('x', x, 'y', y)
-        io.imshow(img)
-        io.show()
-        io.imshow(labels)
-        io.show()
+print(b)
