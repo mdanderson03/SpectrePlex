091b5f427d00ea3982371485c47acae1781718a8
M.D. Anderson <48689483+mdanderson03@users.noreply.github.com>
2026-07-21 09:52:48 -0400

    update

--- Software/use_this.py
+++ Software/use_this.py
@@ -17,12 +17,18 @@ focus_position = 155
 #microscope.wide_net_auto_focus(experiment_directory, x_frame_size=x_frame_size, offset_array=offset_array, z_slice_search_range=9, focus_position=focus_position, number_clusters_retained=5, manual_cluster_update=1)
 
 #uncomment to get autofluorescence
-microscope.full_cycle(experiment_directory, 0, offset_array, 0, pump, z_slices, incub_val = 45, x_frame_size=x_frame_size,focus_position=focus_position)
+#microscope.full_cycle(experiment_directory, 0, offset_array, 0, pump, z_slices, incub_val = 45, x_frame_size=x_frame_size,focus_position=focus_position)
 
 
 
-subprocess.run(['python','partial_cycle.py', str(1), str(2), experiment_directory], capture_output=True, text=True)
+result = subprocess.run(['python','partial_cycle.py', str(1), str(2), experiment_directory], capture_output=True, text=True)
 
+print("Partial-cycle return code:", result.returncode)
+print("Partial-cycle output:", result.stdout)
+print("Partial-cycle errors:", result.stderr)
+
+
+'''
 for cycle in range(2,12):
      stain_vial_num = cycle + 1
      subprocess.run(['python','full_cycle.py', str(stain_vial_num), str(cycle), experiment_directory], capture_output=True, text=True)
@@ -33,3 +39,4 @@ cycles = [0,2,3,4,5,6,7,8,9,10,11]
 for cycle in cycles:
     microscope.inter_cycle_processing(experiment_directory, cycle, x_frame_size=x_frame_size)
 microscope.archive(experiment_directory)
+'''
