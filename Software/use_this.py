df749eb0579c9df8567d81cff90b02ef5024b455
M.D. Anderson <48689483+mdanderson03@users.noreply.github.com>
2026-07-21 14:05:00 -0400

    update

--- Software/use_this.py
+++ Software/use_this.py
@@ -26,13 +26,13 @@ focus_position = 155
 
 
 
-'''
+
 for cycle in range(2,12):
      stain_vial_num = cycle + 1
      subprocess.run(['python','full_cycle.py', str(stain_vial_num), str(cycle), experiment_directory], capture_output=True, text=True)
-'''
 
-microscope.inter_cycle_processing(experiment_directory, 1, x_frame_size=x_frame_size)
+
+#microscope.inter_cycle_processing(experiment_directory, 1, x_frame_size=x_frame_size)
 #cycles = [0,2,3,4,5,6,7,8,9,10,11]
 #for cycle in cycles:
 #    microscope.inter_cycle_processing(experiment_directory, cycle, x_frame_size=x_frame_size)
